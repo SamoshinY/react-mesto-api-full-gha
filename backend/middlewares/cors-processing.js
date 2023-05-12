96ec3789ac8527a556989e17786e449a52eaf53a
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2023-05-12 16:49:11 +0700

    пробую исправить авторизацию

--- backend/middlewares/cors-processing.js
+++ backend/middlewares/cors-processing.js
@@ -22,11 +22,11 @@ module.exports = (req, res, next) => {
   const requestHeaders = req.headers["access-control-request-headers"];
 
   if (method === "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", origin);
-    res.header("Access-Control-Allow-Credentials", true);
+    // res.header("Access-Control-Allow-Origin", origin);
+    // res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
     res.header("Access-Control-Allow-Headers", requestHeaders);
-    return res.end();
+    // return res.end();
   }
   return next();
 };
