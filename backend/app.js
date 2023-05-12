c926a173d935d158c067eb31a8698ac4200e491c
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2023-05-13 01:41:25 +0700

    подключить cors

--- backend/app.js
+++ backend/app.js
@@ -7,17 +7,18 @@ const helmet = require("helmet");
 
 const { PORT = 3000 } = process.env;
 const app = express();
+const cors = require("cors");
 const { errors } = require("celebrate");
 const router = require("./routes/index");
 const errorsHandler = require("./middlewares/handler-errors");
 const rateLimiter = require("./middlewares/rate-limiter");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
-const corsHandler = require("./middlewares/cors-processing");
+// const corsHandler = require("./middlewares/cors-processing");
 
-app.use(corsHandler);
+// app.use(corsHandler);
 
 mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
-
+app.use(cors());
 app
   .use(express.json())
   .use(helmet())
