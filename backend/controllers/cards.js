a2696c46e9692c8c7e39c36abedd62d29a4ed1f0
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2023-05-13 17:23:40 +0700

    откорректировать формат данных карточек

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -5,7 +5,7 @@ const ForbiddenError = require("../utils/errors/ForbiddenError");
 module.exports.getCards = (req, res, next) => {
   Card.find({})
     .populate(["owner", "likes"])
-    .then((cards) => res.send({ data: cards }))
+    .then((cards) => res.send(cards))
     .catch(next);
 };
 
