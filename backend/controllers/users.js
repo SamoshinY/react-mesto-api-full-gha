2289247d2280c3b0685bacecd96d01258e155d73
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2023-05-14 02:09:46 +0700

    добавить логаут

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -27,9 +27,7 @@ module.exports.login = (req, res, next) => {
 };
 
 module.exports.deleteJwt = (req, res, next) => {
-  res.cookie("jwt", undefined, {
-    maxAge: 3000,
-  });
+  res.cookie("jwt", undefined);
   res.send({ message: "successful exit" });
 };
 
