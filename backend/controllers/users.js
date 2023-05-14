1183e6c691e5a854fc4e858ad80e1f10f4e4c836
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2023-05-15 01:20:55 +0700

    небольшой рефакторинг

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -73,19 +73,6 @@ module.exports.getUser = (req, res, next) => {
   findUser(req, res, req.params.userId, next);
 };
 
-// module.exports.getUser = (req, res, next) => {
-//   User.findById(req.params.userId)
-//     .orFail()
-//     .then((user) => res.send(user))
-//     .catch(next);
-// };
-// module.exports.getCurrentUser = (req, res, next) => {
-//   User.findById(req.user._id)
-//     .orFail()
-//     .then((user) => res.send(user))
-//     .catch(next);
-// };
-
 module.exports.getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.send(users))
